443a3a578caa01feebad44444eba68fc0975f221
Rekha <rekha.singh@opensourcetechnologies.com>
2026-01-07 15:32:07 +0530

    enable for local url

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  //app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'http://localhost:8100', // Ionic
+      'https://your-frontend.vercel.app'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
