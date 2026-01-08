a69f92340b6511a87caf9f07dfef1353d0c5ec23
Rekha <rekha.singh@opensourcetechnologies.com>
2026-01-08 22:23:41 +0530

    cors enable

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ async function bootstrap() {
       'http://127.0.0.1:4200',
       'http://localhost:4200',
       'http://localhost:8100', // Ionic
-      'https://your-frontend.vercel.app'
+      'https://drugmatcher-frontendweb.vercel.app',
+      'https://drugmatcher-frontendweb1.vercel.app',
+      'https://drugmatcher-frontendweb2.vercel.app',
+      'https://drugmatcher-frontendweb3.vercel.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
